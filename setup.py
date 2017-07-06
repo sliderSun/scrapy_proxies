406afb90142023910de094ae8924187bb5cf4437
sunshubing <sunshubing@pz-energy.com>
2017-07-06 17:13:32 +0800

    “代理scrapy”

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys
+from distutils.core import setup
+
+setup(name='scrapy-proxies',
+        version='0.3',
+        description='Scrapy Proxies: random proxy middleware for Scrapy',
+        author='slidersun',
+        author_email='291952004@qq.com',
+        url='https://github.com/aivarsk/scrapy-proxies',
+        packages=['scrapy_proxies'],
+        )
